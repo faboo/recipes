96fea3011f0d27de9707236f375a6a20ba708eb2
Ray Wallace <rwallace@deepkeel.ai>
2024-05-04 00:31:44 -0400

    Fix spelling error

--- api/function_app.py
+++ api/function_app.py
@@ -10,7 +10,7 @@ app = func.FunctionApp()
 @app.route(route='whoami')
 def whoami(req:func.HttpRequest) -> func.HttpResponse:
     claimsBytes = base64.b64decode(req.headers['X-MS-CLIENT-PRINCIPAL'])
-    clames = json.loads(claimsBytes)
+    claims = json.loads(claimsBytes)
 
     response = {'ok': True, 'headers': req.headers, 'claims': claims}
     return func.HttpResponse(json.dumps(response), mimetype="application/json")
