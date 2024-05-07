49984fdf5990b3f3365de51d2ce657965d1024b6
Ray Wallace <rwallace@deepkeel.ai>
2024-05-06 21:33:24 -0400

    Python API

--- api/function_app.py
+++ api/function_app.py
@@ -1,41 +1,139 @@
+from typing import cast, List
 import logging
 import os
 import json
 import base64
 import dataclasses
 import azure.functions as func
+from azure.storage.blob import ContainerClient
 
 app = func.FunctionApp()
 
 @dataclasses.dataclass
 class Identity:
     email:str
-    roles:list[str]
+    roles:List[str]
 
 
-def getIdentity(req:func.HttpRequest) -> Identity:
-    claimsBytes = base64.b64decode(req.headers['X-MS-CLIENT-PRINCIPAL'])
-    claims = json.loads(claimsBytes)
-    identity = Identity(email=claims['userDetails'], roles=claims['userRoles'])
+def getIdentity(req:func.HttpRequest) -> Identity|None:
+    identity:Identity|None = None
+    try:
+        claimsBytes = base64.b64decode(req.headers['X-MS-CLIENT-PRINCIPAL'])
+        claims = json.loads(claimsBytes)
+        identity = Identity(email=claims['userDetails'], roles=claims['userRoles'])
+    except Exception:
+        logging.exception('No ID found, assuming anonymous')
+
     return identity
+
+
+class Recipes:
+    def __init__(self, identity:Identity|None) -> None:
+        self.identity = identity
+        self.container = ContainerClient(
+            'https://%s.blob.core.windows.net' % os.getenv('blob_id'),
+            'recipe',
+            os.getenv('blob_key'))
+
+
+    def upsert(self, recipe:dict) -> None:
+        identity = cast(Identity, self.identity)
+        id = recipe['id']
+        with self.container.get_blob_client(f'{identity.email}/{id}.json') as blob:
+            blob.upload_blob(json.dumps(recipe).encode('utf-8'))
+
+
+    def list(self) -> List[dict]:
+        identity = cast(Identity, self.identity)
+        blobs = self.container.list_blobs(name_starts_with=f'{identity.email}/')
+        recipes:List[dict] = []
+
+        for blobProps in blobs:
+            with self.container.get_blob_client(blob=blobProps.name) as blob:
+                content = blob.download_blob(encoding='utf-8')
+                recipes.append(json.load(content))
+
+        return recipes
+
+
+    def get(self, id:str) -> dict:
+        name = f'{self.identity.email}/{id}.json' \
+            if self.identity \
+            else f'{id}.json'
+
+        with self.container.get_blob_client(name) as blob:
+            content = blob.download_blob(encoding='utf-8')
+            return json.load(content)
+
+
+    def delete(self, id:str) -> None:
+        identity = cast(Identity, self.identity)
+        with self.container.get_blob_client(f'{identity.email}/{id}.json') as blob:
+            blob.delete_blob()
+
+
+    def __enter__(self) -> 'Recipes':
+        return self
+
+
+    def __exit__(self, exctype, excval, exctb) -> None:
+        self.shutdown()
+
+
+    def shutdown(self) -> None:
+        self.container.close()
 
 
 @app.route(route='whoami')
 def whoami(req:func.HttpRequest) -> func.HttpResponse:
+    response:dict
     try:
-        #claimsBytes = base64.b64decode(req.headers['X-MS-CLIENT-PRINCIPAL'])
-        #claims = json.loads(claimsBytes)
-        #email = req.headers['X-MS-CLIENT-PRINCIPAL-NAME']
         identity = getIdentity(req)
 
-        response = {'ok': True, 'response': dataclasses.asdict(identity) }
+        response = \
+            { 'ok': True
+            , 'response': dataclasses.asdict(identity) if identity else None
+            }
     except Exception as ex:
         response = {'ok': False, 'response': str(ex) }
 
     return func.HttpResponse(json.dumps(response), mimetype="application/json")
-        
+
 
 @app.route(route='upsert')
 def upsert(req:func.HttpRequest) -> func.HttpResponse:
+    request = req.get_json()
+    with Recipes(getIdentity(req)) as store:
+        store.upsert(request)
+
+    response = {'ok': True}
+    return func.HttpResponse(json.dumps(response), mimetype="application/json")
+
+
+@app.route(route='list')
+def list(req:func.HttpRequest) -> func.HttpResponse:
+    with Recipes(getIdentity(req)) as store:
+        recipes = store.list()
+
+    response = {'ok': True, 'result': recipes}
+    return func.HttpResponse(json.dumps(response), mimetype="application/json")
+
+
+@app.route(route='get')
+def get(req:func.HttpRequest) -> func.HttpResponse:
+    request = req.get_json()
+    with Recipes(getIdentity(req)) as store:
+        recipe = store.get(request['id'])
+    
+    response = {'ok': True, 'result': recipe}
+    return func.HttpResponse(json.dumps(response), mimetype="application/json")
+
+
+@app.route(route='delete')
+def delete(req:func.HttpRequest) -> func.HttpResponse:
+    request = req.get_json()
+    with Recipes(getIdentity(req)) as store:
+        store.delete(request['id'])
+    
     response = {'ok': True}
     return func.HttpResponse(json.dumps(response), mimetype="application/json")
